72b5f7cfc608f7e0da84ae1583af48b4ace75afb
= <aandrewduong@protonmail.com>
2024-10-28 00:24:42 -0700

    API
    API commit

---
+++ api/bruingym-activity-level/README.md
@@ -0,0 +1,42 @@
+# UCLA Bruin "Gym" Activity Level API
+
+This is an API built on the data provided by [UCLA's Connect2Concepts.com](https://connect2concepts.com/connect2/?type=bar&facility=802&key=73829a91-48cb-4b7b-bd0b-8cf4134c04cd) data that scrapes the HTML content and puts it into
+JSON format.
+
+Again, the data was retrieved through UCLA and Connect2Concepts directly.
+
+# Run the application
+    go run .
+
+# Build
+    go build main.go
+
+## Structure
+```
+├── bruingym-activity-level
+│   ├── .env              // Transfercamp Data
+│   ├── build.sh         /// Build shell script
+│   └── main.go
+```
+
+# API
+Endpoint: http://localhost:1337
+
+/facility
+* 'GET': Get the facility data
+
+`Query`
+`facility`
+
+Possible `facility` query inputs
+`jwc` `bfit` `krc`
+
+Example
+Get the facility data for a specific facility such as the John Wooden Center
+
+`GET /facility`
+    curl "http://localhost:1337/facility?facility=jwc
+
+### Response
+    Content-Type: application/json
+    [{"name":"Cardio Zone","percentage":"25%"},{"name":"Free Weight Zone","percentage":"29%"},{"name":"Functional Training Zone","percentage":"10%"},{"name":"Courtyard","percentage":"16%"},{"name":"Advanced Circuit Zone","percentage":"38%"},{"name":"Novice Circuit Zone","percentage":"15%"},{"name":"Pardee Gym","percentage":"2%"},{"name":"*Collins Court","percentage":"33%"}]
---
+++ api/bruingym-activity-level/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/joho/godotenv"
+)
+
+type ZoneData struct {
+	Name       string `json:"name"`
+	Percentage string `json:"percentage"`
+}
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		return
+	}
+
+	http.HandleFunc("/facility", handleFacility)
+
+	port := ":1337"
+	fmt.Printf("Starting server on port %s\n", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		fmt.Printf("Error starting server: %v\n", err)
+	}
+}
+
+func handleFacility(w http.ResponseWriter, r *http.Request) {
+	facility := r.URL.Query().Get("facility")
+	if facility == "" {
+		http.Error(w, "Facility query is required", http.StatusBadRequest)
+		return
+	}
+
+	var facilityNumber string
+	switch strings.ToLower(facility) {
+	case "jwc":
+		facilityNumber = "802"
+	case "bfit":
+		facilityNumber = "803"
+	case "krc":
+		facilityNumber = "804"
+	default:
+		http.Error(w, "Invalid facility. Use 'jwc', 'bfit' or 'krc'.", http.StatusBadRequest)
+		return
+	}
+
+	key := os.Getenv("KEY")
+	if key == "" {
+		http.Error(w, "API key not found", http.StatusInternalServerError)
+		return
+	}
+	url := "https://connect2concepts.com/connect2/?type=bar&facility=" + facilityNumber + "&key=" + key
+
+	zones, err := scrapeBarCharts(url)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error scraping bar charts: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(zones)
+}
+
+func scrapeBarCharts(url string) ([]ZoneData, error) {
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("Error creating request: %v", err)
+	}
+
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8")
+	req.Header.Set("Accept-Language", "en-US,en;q=0.7")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("Error making request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Request failed with status code: %d", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Error reading response body: %v", err)
+	}
+
+	document, err := goquery.NewDocumentFromReader(strings.NewReader(string(body)))
+	if err != nil {
+		return nil, fmt.Errorf("Error loading HTML into goquery: %v", err)
+	}
+
+	var zones []ZoneData
+
+	document.Find(".barChart").Each(func(i int, s *goquery.Selection) {
+		zoneName := strings.TrimSpace(s.Contents().First().Text())
+		percentage := s.Find(".barChart__value").Text()
+
+		zones = append(zones, ZoneData{Name: zoneName, Percentage: percentage})
+	})
+
+	return zones, nil
+}
