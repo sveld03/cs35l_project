396a43962058ba25197e6b72b8b9a30eb65b219d
AnthonyChui <154588467+AnthonyChui@users.noreply.github.com>
2024-11-17 18:49:01 -0800

    Updated Animated Text

--- healthyonthehill/src/components/AnimatedText.js
+++ healthyonthehill/src/components/AnimatedText.js
@@ -1,25 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import TypewriterComponent from 'typewriter-effect';
-const wordList = ['Meal', 'Exercise', 'Bruin Buddy', 'Meal'];
 
 const AnimatedText = () => {
-    const [currWord, setCurrWord] = useState(wordList[0]);
-
-    const index = useRef(0);
-    useEffect(() => {
-        let interval = null;
-        if (true) {
-            interval = setInterval(() => {
-                index.current++;
-                setCurrWord(wordList[index.current]);
-                if (index.current === wordList.length - 1) {
-                    index.current = 0;
-                }
-            }, 1200);
-        }
-        return () => clearInterval(interval);
-    });
-
     return (
         <div style={{ display: 'flex', alignItems: 'center' }}>
             <h2 style={{ margin: 0 }}>Find your Favorite&nbsp;</h2>
