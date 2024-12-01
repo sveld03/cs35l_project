87768aa931b0c21b36b0a0f8b2b16747c1466a0f
4n4ny4 <ri82ananya@gmail.com>
2024-12-01 15:22:40 -0800

    Added more users to new database

--- HOTH-server/routes/authRoutes.js
+++ HOTH-server/routes/authRoutes.js
@@ -2,11 +2,14 @@
 
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser, verifyToken } = require('../controllers/authController');
+const { registerUser, loginUser, verifyToken, registerUsers } = require('../controllers/authController');
 
 // Auth routes
 router.post('/register', registerUser);
+router.post('/registerAll', registerUsers);
 router.post('/login', loginUser);
 router.post('/verifyUser', verifyToken);
+
+
 
 module.exports = router;
