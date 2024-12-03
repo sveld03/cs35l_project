aafe7dd301435d39079550b63da50bb041e14a19
gabrielscharff@gmail.com <gabrielscharff@gmail.com>
2024-12-03 13:23:14 -0800

    added getAccount and updateAccount to access and update account information

--- HOTH-server/routes/authRoutes.js
+++ HOTH-server/routes/authRoutes.js
@@ -2,13 +2,15 @@
 
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser, verifyToken, registerUsers } = require('../controllers/authController');
+const { registerUser, loginUser, verifyToken, registerUsers, getAccount, updateAccount } = require('../controllers/authController');
 
 // Auth routes
 router.post('/register', registerUser);
 router.post('/registerAll', registerUsers);
 router.post('/login', loginUser);
 router.post('/verifyUser', verifyToken);
+router.post('/getAccount', getAccount);
+router.post('/updateAccount', updateAccount);
 
 
 
