93c16c25f938b24c8b47daca7a5be0ba4bc33d6c
gabrielscharff@gmail.com <gabrielscharff@gmail.com>
2024-12-03 15:58:23 -0800

    UI cleanup

--- healthyonthehill/src/pages/RecommendedMachines.js
+++ healthyonthehill/src/pages/RecommendedMachines.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import NavBar from '../components/NavBar';
-
 import MachineForm from './MachineForm';
 import MachineFilter from './MachineFilter';
 
@@ -18,21 +16,10 @@ const RecommendedMachines = () => {
     return (
         <Box>
             <NavBar />
-            
-            {/* Page Header */}
-            <Box sx={{ p: 4 }}>
-                <Typography variant="h4" gutterBottom>
-                    Gym Equipment Directory
-                </Typography>
-                <Typography variant="subtitle1" color="text.secondary" gutterBottom>
-                    Browse all Equipment at the Bruin Fitness Center
-                </Typography>
-            </Box>
-            <div>
-                <h1>Fitness Equipment Recommendation</h1>
-                <MachineForm onSubmit={handleFormSubmit} />
-                {preferences && <MachineFilter preferences={preferences} />}
-            </div>
+
+            <MachineForm onSubmit={handleFormSubmit} />
+            {preferences && <MachineFilter preferences={preferences} />}
+
         </Box>
     );
 };
